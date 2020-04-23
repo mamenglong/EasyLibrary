6a0fd007101ee9d9d16baf99b639b25ed789e363
menglong <mml2015@126.com>
2020-04-23 18:34:53 +0800

    feat:增加方法

--- kotlinextension/src/main/java/com/mml/kotlinextension/ScopedStorage.kt
+++ kotlinextension/src/main/java/com/mml/kotlinextension/ScopedStorage.kt
@@ -75,6 +75,20 @@ fun pickMultipleFiles(context: FragmentActivity, requestCode: Int=READ_MULTIPLE_
 }
 
 /**
+ * 从图库选择图片 使用  ACTION_PICK
+ */
+fun pickImageFromGallery(context: FragmentActivity, requestCode: Int=READ_MULTIPLE_REQUEST_CODE,isMultiple: Boolean = false){
+    val intent = Intent(
+        Intent.ACTION_PICK
+    )
+    intent.data= MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+    if (isMultiple) {
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+    }
+    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    context.startActivityForResult(intent, requestCode)
+}
+/**
  * 打开文件管理器选择文件夹
  * 在 [onActivityResult] 获取   uri = data.data
  * @param context
